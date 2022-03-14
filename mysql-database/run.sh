ec4c516f7418e24726fba2d3aa49f71bf333bb16
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-03-14 19:17:11 -0300

    Check if variables exists

--- mysql-database/run.sh
+++ mysql-database/run.sh
@@ -2,6 +2,18 @@
 
 BACKUP_FILENAME=./mysql-database/backup/$(date +'%Y-%m-%d').sql
 
+check_var() {
+    if test -z "$2"; then
+        echo "Unknown \$$1 Variable" >&2
+        exit 1
+    fi
+}
+
+check_var 'MYSQL_HOST' "$MYSQL_HOST"
+check_var 'MYSQL_PORT' "$MYSQL_PORT"
+check_var 'MYSQL_USERNAME' "$MYSQL_USERNAME"
+check_var 'MYSQL_PASSWORD' "$MYSQL_PASSWORD"
+
 mysqldump \
     --host "$MYSQL_HOST" \
     --port "$MYSQL_PORT" \
